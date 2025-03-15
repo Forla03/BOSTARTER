27b9c65fb2f7ef8cb278973b10e7a8fba630b2d7
Forla03 <fraforla2003@gmail.com>
2025-03-15 17:21:03 +0100

    Path correction

--- home.js
+++ home.js
@@ -2,17 +2,17 @@ document.addEventListener("DOMContentLoaded", function () {
     const loginBtn = document.getElementById("loginBtn");
 
     // Effettua una richiesta AJAX per controllare lo stato della sessione
-    fetch("../../Backend/session.php")
+    fetch("/Backend/session.php")
         .then(response => response.json())
         .then(data => {
             if (data.logged_in) {
-                // L'utente è loggato: "Logout (nickname)"
+                // L'utente ï¿½ loggato: "Logout (nickname)"
                 loginBtn.textContent = `Logout (${data.nickname})`;
                 loginBtn.addEventListener("click", function () {
                     window.location.href = "/Backend/logout.php";
                 });
             } else {
-                // L'utente non è loggato:  "Accedi"
+                // L'utente non ï¿½ loggato:  "Accedi"
                 loginBtn.textContent = "Accedi";
                 loginBtn.addEventListener("click", function () {
                     window.location.href = "/Frontend/login/login.html";
